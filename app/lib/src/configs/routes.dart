6db0a4bce438146edbd9d1e1fe40b0959818c78b
Gabriel Martín Moran <moran.gabriel.95@gmail.com>
2021-04-16 14:47:53 -0300

    Mejoras al routeo

--- app/lib/src/configs/routes.dart
+++ app/lib/src/configs/routes.dart
@@ -1,11 +1,16 @@
 import 'package:app/src/screens/devices/add_device_screen.dart';
+import 'package:app/src/screens/devices/devices_screen.dart';
+import 'package:app/src/screens/devices/view_device_screen.dart';
 import 'package:app/src/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
   static final Map routes = {
     '/': () => HomeScreen(),
-    '/add_device': () => AddDeviceScreen(),
+    '/devices': () => DevicesScreen(),
+    '/devices/add': () => AddDeviceScreen(),
+    '/devices/view/<int>': (List<dynamic> args) =>
+        ViewDeviceScreen(deviceId: args[0]),
   };
 
   static String routeOf(Widget widgetName) {
